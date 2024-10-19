b364ddafc67875060901db9b8677c49ac3e406a0
aja0010101 <61653782+aja0010101@users.noreply.github.com>
2024-10-19 21:25:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,8 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+rm -rf feeds/small/luci-app-passwall
+git clone https://github.com/simonchen/luci-app-passwall.git feeds/small/luci-app-passwall
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
