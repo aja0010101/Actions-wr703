5b01f45b5da5529b36519cfc939508d6b966226c
aja0010101 <61653782+aja0010101@users.noreply.github.com>
2024-12-08 20:28:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,20 @@
 # rm -rf feeds/small/luci-app-passwall
 # git clone https://github.com/simonchen/luci-app-passwall.git feeds/small/luci-app-passwall
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
+
+rm -rf feeds/luci/themes/luci-theme-argon
+
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+
+./scripts/feeds update -i
+./scripts/feeds install -a
+set -x
+
+cd package
+sed -i "s/LEDE /We build $(TZ=UTC-8 date "+%Y.%m.%d") @ LEDE /g" lean/default-settings/files/zzz-default-settings
+sed -i "s/LEDE/MI-R3G/" base-files/luci2/bin/config_generate
+sed -i "/firewall\.user/d" lean/default-settings/files/zzz-default-settings
+sed -i "s/192.168.1.1/192.168.123.1/g" base-files/luci2/bin/config_generate
+sed -i "/openwrt_luci/d" lean/default-settings/files/zzz-default-settings
