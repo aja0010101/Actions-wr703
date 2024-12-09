dbe01c3fe7e63073eac3d5dc75338bd3598bb1e9
aja0010101 <61653782+aja0010101@users.noreply.github.com>
2024-12-09 21:10:23 +0800

    Update diy-part2-235.sh

--- diy-part2-235.sh
+++ diy-part2-235.sh
@@ -25,7 +25,4 @@ set -x
 
 cd package
 sed -i "s/LEDE /We build $(TZ=UTC-8 date "+%Y.%m.%d") @ LEDE /g" lean/default-settings/files/zzz-default-settings
-sed -i "s/LEDE/MI-R3G/" base-files/luci2/bin/config_generate
-sed -i "/firewall\.user/d" lean/default-settings/files/zzz-default-settings
-sed -i "s/192.168.1.1/192.168.123.1/g" base-files/luci2/bin/config_generate
-sed -i "/openwrt_luci/d" lean/default-settings/files/zzz-default-settings
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
