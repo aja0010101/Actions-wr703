21b9701840c7de401c29544b24b9e29d6c5c5e5e
aja0010101 <61653782+aja0010101@users.noreply.github.com>
2024-12-11 05:49:18 +0800

    Update diy-part233.sh

--- diy-part233.sh
+++ diy-part233.sh
@@ -14,17 +14,17 @@
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
 
-# rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-argon
 
-# git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-# git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
-# ./scripts/feeds update -i
-# ./scripts/feeds install -a
-# set -x
+./scripts/feeds update -i
+./scripts/feeds install -a
+set -x
 
 # cd package
-sed -i "s/LEDE /We build $(TZ=UTC-8 date "+%Y.%m.%d") @ LEDE /g" lean/default-settings/files/zzz-default-settings
+# sed -i "s/LEDE /We build $(TZ=UTC-8 date "+%Y.%m.%d") @ LEDE /g" lean/default-settings/files/zzz-default-settings
 # sed -i "s/LEDE/MI-R3G/" base-files/luci2/bin/config_generate
 # sed -i "/firewall\.user/d" lean/default-settings/files/zzz-default-settings
 # sed -i "s/192.168.1.1/192.168.123.1/g" base-files/luci2/bin/config_generate
