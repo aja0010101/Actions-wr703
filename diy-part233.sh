e416c3b03eb70f034f5bac820d0c06262a5cf12e
aja0010101 <61653782+aja0010101@users.noreply.github.com>
2024-12-20 20:33:34 +0800

    Update diy-part233.sh

--- diy-part233.sh
+++ diy-part233.sh
@@ -33,6 +33,139 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
+color() {
+    case $1 in
+        cr) echo -e "\e[1;31m$2\e[0m" ;;
+        cg) echo -e "\e[1;32m$2\e[0m" ;;
+        cy) echo -e "\e[1;33m$2\e[0m" ;;
+        cb) echo -e "\e[1;34m$2\e[0m" ;;
+        cp) echo -e "\e[1;35m$2\e[0m" ;;
+        cc) echo -e "\e[1;36m$2\e[0m" ;;
+    esac
+}
+
+status() {
+    local check=$? end_time=$(date '+%H:%M:%S') total_time
+    total_time="==> 用时 $[$(date +%s -d $end_time) - $(date +%s -d $begin_time)] 秒"
+    [[ $total_time =~ [0-9]+ ]] || total_time=""
+    if [[ $check = 0 ]]; then
+        printf "%-62s %s %s %s %s %s %s %s\n" \
+        $(color cy $1) [ $(color cg ✔) ] $(echo -e "\e[1m$total_time")
+    else
+        printf "%-62s %s %s %s %s %s %s %s\n" \
+        $(color cy $1) [ $(color cr ✕) ] $(echo -e "\e[1m$total_time")
+    fi
+}
+
+find_dir() {
+    find $1 -maxdepth 3 -type d -name $2 -print -quit 2>/dev/null
+}
+
+print_info() {
+    printf "%s %-40s %s %s %s\n" $1 $2 $3 $4 $5
+    # read -r param1 param2 param3 param4 param5 <<< $1
+    # printf "%s %-40s %s %s %s\n" $param1 $param2 $param3 $param4 $param5
+}
+
+# 添加整个源仓库(git clone)
+git_clone() {
+    local repo_url branch target_dir current_dir
+    if [[ "$1" == */* ]]; then
+        repo_url="$1"
+        shift
+    else
+        branch="-b $1 --single-branch"
+        repo_url="$2"
+        shift 2
+    fi
+    if [[ -n "$@" ]]; then
+        target_dir="$@"
+    else
+        target_dir="${repo_url##*/}"
+    fi
+    git clone -q $branch --depth=1 $repo_url $target_dir 2>/dev/null || {
+        print_info $(color cr 拉取) $repo_url [ $(color cr ✕) ]
+        return 0
+    }
+    rm -rf $target_dir/{.git*,README*.md,LICENSE}
+    current_dir=$(find_dir "package/ feeds/ target/" "$target_dir")
+    if ([[ -d $current_dir ]] && rm -rf $current_dir); then
+        mv -f $target_dir ${current_dir%/*}
+        print_info $(color cg 替换) $target_dir [ $(color cg ✔) ]
+    else
+        mv -f $target_dir $destination_dir
+        print_info $(color cb 添加) $target_dir [ $(color cb ✔) ]
+    fi
+}
+
+# 添加源仓库内的指定目录
+clone_dir() {
+    local repo_url branch temp_dir=$(mktemp -d)
+    if [[ "$1" == */* ]]; then
+        repo_url="$1"
+        shift
+    else
+        branch="-b $1 --single-branch"
+        repo_url="$2"
+        shift 2
+    fi
+    git clone -q $branch --depth=1 $repo_url $temp_dir 2>/dev/null || {
+        print_info $(color cr 拉取) $repo_url [ $(color cr ✕) ]
+        return 0
+    }
+    local target_dir source_dir current_dir
+    for target_dir in "$@"; do
+        source_dir=$(find_dir "$temp_dir" "$target_dir")
+        [[ -d $source_dir ]] || \
+        source_dir=$(find $temp_dir -maxdepth 4 -type d -name $target_dir -print -quit) && \
+        [[ -d $source_dir ]] || {
+            print_info $(color cr 查找) $target_dir [ $(color cr ✕) ]
+            continue
+        }
+        current_dir=$(find_dir "package/ feeds/ target/" "$target_dir")
+        if ([[ -d $current_dir ]] && rm -rf $current_dir); then
+            mv -f $source_dir ${current_dir%/*}
+            print_info $(color cg 替换) $target_dir [ $(color cg ✔) ]
+        else
+            mv -f $source_dir $destination_dir
+            print_info $(color cb 添加) $target_dir [ $(color cb ✔) ]
+        fi
+    done
+    rm -rf $temp_dir
+}
+
+# 添加源仓库内的所有目录
+clone_all() {
+    local repo_url branch temp_dir=$(mktemp -d)
+    if [[ "$1" == */* ]]; then
+        repo_url="$1"
+        shift
+    else
+        branch="-b $1 --single-branch"
+        repo_url="$2"
+        shift 2
+    fi
+    git clone -q $branch --depth=1 $repo_url $temp_dir 2>/dev/null || {
+        print_info $(color cr 拉取) $repo_url [ $(color cr ✕) ]
+        return 0
+    }
+    local target_dir source_dir current_dir
+    for target_dir in $(ls -l $temp_dir/$@ | awk '/^d/{print $NF}'); do
+        source_dir=$(find_dir "$temp_dir" "$target_dir")
+        current_dir=$(find_dir "package/ feeds/ target/" "$target_dir")
+        if ([[ -d $current_dir ]] && rm -rf $current_dir); then
+            mv -f $source_dir ${current_dir%/*}
+            print_info $(color cg 替换) $target_dir [ $(color cg ✔) ]
+        else
+            mv -f $source_dir $destination_dir
+            print_info $(color cb 添加) $target_dir [ $(color cb ✔) ]
+        fi
+    done
+    rm -rf $temp_dir
+}
+
+# clone_dir openwrt-23.05 https://github.com/coolsnowwolf/luci luci-app-adguardhome
+
 # git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser luci-app-ssr-mudb-server
 # git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/luci-app-eqos
 # git_sparse_clone main https://github.com/haiibo/packages luci-theme-atmaterial luci-theme-opentomcat luci-theme-netgear
@@ -41,7 +174,9 @@ function git_sparse_clone() {
 #git_sparse_clone master https://github.com/linkease/nas-packages network/services/ddnsto
 
 rm -rf feeds/kenzo/luci-app-filebrowser
-git clone https://github.com/OldCoding/luci-app-filebrowser package/luci-app-filebrowser
+rm -rf package/luci-app-filebrowser
+# git_sparse_clone master https://github.com/haiibo/openwrt-packages luci-app-filebrowser
+git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser
 rm -rf package/lean/luci-theme-argon 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
 
