d6a4b9504a5b5db4602fc7e0176feec6f06a90d6
aja0010101 <61653782+aja0010101@users.noreply.github.com>
2024-12-22 09:03:37 +0800

    Update diy-part233.sh

--- diy-part233.sh
+++ diy-part233.sh
@@ -176,17 +176,19 @@ clone_all() {
 rm -rf feeds/kenzo/luci-app-filebrowser
 rm -rf package/luci-app-filebrowser
 rm -rf feeds/kenzo/filebrowser
-git_sparse_clone main https://github.com/kiddin9/kwrt-packages filebrowser
-git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-filebrowser-go
-git_sparse_clone main https://github.com/kiddin9/kwrt-packages dufs
-git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-dufs
+# git_sparse_clone main https://github.com/kiddin9/kwrt-packages filebrowser
+# git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-filebrowser-go
+git_sparse_clone main https://github.com/kenzok8/jell dufs
+git_sparse_clone main https://github.com/kenzok8/jell luci-app-dufs
 git_sparse_clone master https://github.com/haiibo/openwrt-packages gowebdav
 git_sparse_clone master https://github.com/haiibo/openwrt-packages luci-app-gowebdav
 
 # git_sparse_clone master https://github.com/kiddin9/kwrt-packages luci-app-dufs
 # git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser
-rm -rf package/lean/luci-theme-argon 
+rm -rf package/lean/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
 # cd package
 # mkdir kwrt-packages
